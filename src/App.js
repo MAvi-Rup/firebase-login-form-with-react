ca399b3f8f1b8e0e0ebf64e9bd8f2d09456de9d0
MAvi-Rup <avirupmondal19@gmail.com>
2022-04-11 01:44:04 +0600

    Simple login form using react and firebase

---
+++ src/App.js
@@ -0,0 +1,81 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from './firbase.init';
+import { Button, Form } from 'react-bootstrap';
+import {useState} from 'react'
+
+
+const auth = getAuth(app)
+function App() {
+  const [validated, setValidated] = useState(false);
+  const [error,setError]=useState('')
+  const [email,setEmail]=useState('')
+  const [pass,setPass]= useState('')
+  const handleSubmit = e=>{
+    e.preventDefault();
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+      return;
+    }
+    if(!/(?=.*[!@#$%^&*])/.test(pass)){
+      setError('Password Should contain atlease one special charecter.')
+      return;
+    }
+    setError('')
+    setValidated(true);
+    createUserWithEmailAndPassword(auth, email,pass)
+    .then(result =>{
+      const user = result.user;
+      console.log(user)
+    })
+    .catch(error =>{
+      console.error(error)
+    })
+    e.preventDefault()
+  }
+  const handleEmailBlur = e=>{
+    setEmail(e.target.value)
+  }
+  const handlePassBlur = e=>{
+    setPass(e.target.value)
+  }
+  return (
+    <div className="container w-50 mt-5">
+      <h2 className='text-info'>Please Register</h2>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid email.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control onBlur={handlePassBlur}  type="password" placeholder="Password" required />
+        </Form.Group>
+        <Form.Control.Feedback type="invalid">
+            Please provide a valid password.
+          </Form.Control.Feedback>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <p className='text-danger'>{error}</p>
+        <Button  variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firbase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyq6yHwhKUdQtgLj3ndK4ZvDfeO0Gv5X8",
+  authDomain: "email-password-auth-27be8.firebaseapp.com",
+  projectId: "email-password-auth-27be8",
+  storageBucket: "email-password-auth-27be8.appspot.com",
+  messagingSenderId: "72846470130",
+  appId: "1:72846470130:web:cdcf38217c101768b3196b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
